9c47086a904f458f29a17c4880a6f365d1aa5d28
IIJaredII <jaredrios2425@gmail.com>
2025-03-18 15:33:09 -0600

    -Creacion de la funcions para subir ficheros de tipo imagen para los perfiles de los clientes. -cambios en las rutas  y en el controlador de los clientes

--- src/routes/cliente.routes.js
+++ src/routes/cliente.routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const clienteController = require("../controllers/cliente.controller");
+const upload = require("../middlewares/multerConfig");
 
-router.post("/", clienteController.insertarCliente);
-router.put("/:id", clienteController.actualizarCliente);
+router.post("/", upload.single("foto"), clienteController.insertarCliente);
+router.put("/:id", upload.single("foto"), clienteController.actualizarCliente);
 router.delete("/:id", clienteController.eliminarCliente);
 router.get("/", clienteController.obtenerClientes);
 router.get("/:id", clienteController.obtenerClientePorID);
