25d0a3890864579670358484f4415427c3779e58
FranklinTS2 <148499845+FranklinTS2@users.noreply.github.com>
2025-03-29 09:55:32 -0600

    Creación del controlador y las rutas de carros

---
+++ src/controllers/carro.controller.js
@@ -0,0 +1,140 @@
+const connection = require('../config/db');
+
+const insertarCarro = async (req,res) => {
+    try{
+        const {idCliente, placa, modelo, year} = req.body;
+        
+        if(!idCliente || !placa || !modelo || !year){
+            return res.status(400).json({mensaje: "Todos los campos son obligatorios"})
+        }
+
+        const [carros] = await connection.promise().query("SELECT id FROM carros where placa=?",[placa]);
+
+        if(carros.length>0){
+            return res.status(409).json({mensaje: "El carro ya está registrado"});
+        }
+
+        const results = await connection.promise().query("CALL RegistrarCarro(?,?,?,?)",[idCliente,placa,modelo,year]);
+
+        res.status(201).json({mensaje: "Carro insertado exitosamente",
+            id: results.insertId
+        });
+
+    }catch(error){
+        console.error("Error al insertar carro: ",error);
+        res.status(500).json({mensaje: "Error al insertar carro"});
+    }
+}
+
+const obtenerCarroPorIdCliente = async (req,res) => {
+    try{
+        const {id} = req.body;
+    
+        const [results] = await connection.promise().query("CALL ObtenerCarrosPorCliente(?)",[id]);
+
+        if(!results[0] || results[0].length === 0){
+            return res.status(404).json({mensaje: "Carro no encontrado"});
+        }
+
+        res.json(results[0]);
+
+    }catch(error){
+        console.error("Error al obtener carro por id cliente: ",error);
+        return res.status(500).json({mensaje: "Error al obtener carro por id cliente"});
+    }
+}
+
+const obtenerCarroPorId = async (req,res) => {
+    try{
+        const {id} = req.body;
+    
+        const [results] = await connection.promise().query("CALL ObtenerCarroPorID(?)",[id]);
+
+        if(!results[0] || results[0].length === 0){
+            return res.status(404).json({mensaje: "Carro no encontrado"});
+        }
+
+        res.json(results[0]);
+
+    }catch(error){
+        console.error("Error al obtener carro por id: ",error);
+        return res.status(500).json({mensaje: "Error al obtener carro por id"});
+    }
+}
+
+const obtenerCarros = async (req,res) => {
+    try{
+        const [results] = await connection.promise().query('SELECT * FROM carros WHERE estado=1');
+        res.json(results[0]);
+
+    }catch(error){
+        console.error("Error al obtener carros: ",error);
+        res.status(500).json({mensaje: "Error al obtener carros"});
+
+    }
+};
+
+const actualizarCarro = async (req,res) => {
+    try{
+        const {id, idCliente, placa, modelo, year} = req.body;
+        
+        if(!id || !idCliente || !placa || !modelo || !year){
+            return res.status(400).json({mensaje: "Todos los campos son obligatorios"})
+        }
+
+        const [carro] = await connection.promise().query("SELECT placa FROM carros WHERE id=?",[id]);
+
+        if(carro.length === 0){
+            return res.status(409).json({mensaje: "El carro no ha sido encontrado"});
+        }
+
+        await connection.promise().query("CALL ActualizarCarro(?,?,?,?,?)", [id, idCliente, placa, modelo, year]);
+        res.json({mensaje: "Carro actualizado exitosamente"});
+
+    }catch(error){
+        console.error("Error al actualizar carro: ",error);
+        return res.status(500).json({mensaje: "Error al actualizar carro"});
+    }
+}
+
+const eliminarCarro = async(req,res) => {
+    try{
+        const {id} = req.body;
+
+        await connection.promise().query("CALL eliminarCarro(?)",[id]);
+
+        res.json({mensaje: "Carro eliminado correctamente"});
+
+    }catch(error){
+        console.error("Error al eliminar Carro: ",error);
+        res.status(500).json({mensaje: "Error al eliminar Carro"});
+    }
+}
+
+const obtenerModeloPorIdMarca = async (req,res) => {
+    try{
+        const {id} = req.body;
+    
+        const [results] = await connection.promise().query("CALL ObtenerModelosPorMarca(?)",[id]);
+
+        if(!results[0] || results[0].length === 0){
+            return res.status(404).json({mensaje: "Modelo no encontrado"});
+        }
+
+        res.json(results[0]);
+
+    }catch(error){
+        console.error("Error al obtener modelo por id Marca: ",error);
+        return res.status(500).json({mensaje: "Error al obtener modelo por id Marca"});
+    }
+}
+
+module.exports = {
+    insertarCarro,
+    obtenerCarroPorIdCliente,
+    obtenerCarroPorId,
+    obtenerCarros,
+    actualizarCarro,
+    eliminarCarro,
+    obtenerModeloPorIdMarca
+}
---
+++ src/routes/carro.routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const carroController = require('../controllers/carro.controller');
+
+router.post('/',carroController.insertarCarro);
+router.get('/idCliente',carroController.obtenerCarroPorIdCliente);
+router.get('/id',carroController.obtenerCarroPorId);
+router.get('/',carroController.obtenerCarros);
+router.put('/',carroController.actualizarCarro);
+router.delete('/',carroController.eliminarCarro);
+router.get('/modelo',carroController.obtenerModeloPorIdMarca)
+module.exports = router;
